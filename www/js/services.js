73f2b69fbf5794aca6d714bc9c6f7d6c49f31086
brian.uribe <brian.uribe93@gmail.com>
2016-04-29 20:00:51 -0500

    guardado en base de fireBase

--- www/js/services.js
+++ www/js/services.js
@@ -1,13 +1,21 @@
 angular.module('starter.services', [])
     .factory('Auth', function($firebaseAuth,$rootScope) {
         var usersRef = new Firebase("https//mybankatom.firebaseio.com/users");
+        var users = usersRef.push();
         return {
             iniciarSesionGoogle: function() {
                 usersRef.authWithOAuthPopup("google", function(error, authData) { 
+                    $rootScope.uid = authData.uid,
+                    $rootScope.email = authData.google.email,
                 	$rootScope.nombreUsuario = authData.google.displayName,
                 	$rootScope.token = authData.token,
                 	$rootScope.fotoPerfil = authData.google.profileImageURL,
-                	 }, {
+                    users.set({
+                            email:$rootScope.email, 
+                            name: $rootScope.nombreUsuario, 
+                            token: $rootScope.token,
+                        });
+                }, {
                     remember: "sessionOnly",
                     scope: "email"
                 });
